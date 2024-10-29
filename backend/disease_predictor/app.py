0c9b60f81800d1075c5c4dbd681faf39dfb409ee
Burak Mat <burakk.mat@gmail.com>
2024-10-29 17:11:29 +0300

    model v2

--- backend/disease_predictor/app.py
+++ backend/disease_predictor/app.py
@@ -3,92 +3,38 @@ import pickle
 import pandas as pd
 from flask_cors import CORS
 
-model_path = './model.pkl'
+model_path = './model_bundle.pkl'
 with open(model_path, 'rb') as file:
-    model = pickle.load(file)
+    bundle = pickle.load(file)
+
+model = bundle["model"]
+label_encoder = bundle["label_encoder"]
+encoder = bundle["encoder"]
 
 # create and configure the app
 app = Flask(__name__, instance_relative_config=True)
 CORS(app)
 
-bacteria_names = {
-    0: "vibrio",
-    1: "Yersinia Ruckeri",
-    2: "V.Salmoninarum",
-    3: "Aeromonas spp.",
-    4: "Flavobacter psychrophilum",
-    5: "L.garvieae",
-    6: "Pseudomonas spp."
-}
-
 def preprocess_input(input_data):
-    mappings = {
-        'test1': {'+': [0], '-': [1]},
-        'test2': {'+': [0], '-': [1]},
-        'test3': {'+': [0], '-': [1]},
-        'test4': {'+': [0], '-': [1]},
-        'test5': {'+/+': [0], '-/-': [1]},
-        'test6': {'0': [1,0], '1': [0,0], '2': [0,1]},
-        'test7': {
-            '0': [0,0,0,0,0,0,0], '1': [1,0,0,0,0,0,0],
-            '2': [0,1,0,0,0,0,0],
-            '4': [0,0,1,0,0,0,0], '5': [0,0,0,1,0,0,0],
-            '6': [0,0,0,0,1,0,0], '7': [0,0,0,0,0,1,0],
-            '3': [0,0,0,0,0,0,1]
-        },
-        'test8': {
-            '1': [0,0,0,0],
-            '0': [1,0,0,0],
-            '3': [0,1,0,0],
-            '2': [0,0,1,0],
-            '4': [0,0,0,1]
-        },
-        'test9': {'+': [], '-': []},
-        'test10': {
-            '1': [0,0,0,0,0,0,0],
-            '5': [1,0,0,0,0,0,0],
-            '4': [0,1,0,0,0,0,0],
-            '3': [0,0,1,0,0,0,0],
-            '0': [0,0,0,1,0,0,0],
-            '6': [0,0,0,0,1,0,0],
-            '2': [0,0,0,0,0,1,0],
-            '7': [0,0,0,0,0,0,1]
-        },
-        'test11': {'0': [1], '1': [0]},
-        'test12': {'1': [0,0],
-                    '2': [1,0],
-                    '0': [0,1]},
-        'test13': {True: 1, False:0}
-    }
-
-    processed_data = []
-    for key, value in input_data.items():
-        if key in mappings:
-            mapped_value = mappings[key][value]
-            if isinstance(mapped_value, list):
-                processed_data.extend(mapped_value)
-            else:
-                processed_data.append(mapped_value)
-
-    return pd.DataFrame([processed_data])
-
+    dict_data = {k: [v] for k, v in input_data.items()}
+    df_data = pd.DataFrame(dict_data, columns=encoder.feature_names_in_)
+    encoded_data = encoder.transform(df_data)
+    return pd.DataFrame(encoded_data, columns=encoder.get_feature_names_out(df_data.columns))
 
 @app.route('/predict', methods=['POST'])
 def predict():
     input_data = request.json
-    if input_data.get('test13', 0):
+    if input_data.get('slippage', 0):
         bacteria_name = "Flavobacter psychrophilum"
         return {"prediction": bacteria_name}
         
     else:
-        processed_data = {k: v for k, v in input_data.items() if k != 'test13'}
-        processed_data = preprocess_input(processed_data)
-        processed_data = processed_data.values
-
+        input_data.pop('slippage', None)
+        processed_data = preprocess_input(input_data)
         numerical_prediction = model.predict(processed_data)
-        bacteria_name = bacteria_names.get(numerical_prediction[0])
+        bacteria_name = label_encoder.inverse_transform(numerical_prediction)
 
-        return {"prediction": bacteria_name}
+        return {"prediction": bacteria_name[0]}
             
 if __name__ == '__main__':
     app.run(port=8000)
